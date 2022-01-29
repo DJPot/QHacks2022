48cf0ea56b2a4c904324af487f032bf22b007d4c
Damien Pot (17DJP8) <damien.pot@queensu.ca>
2022-01-29 16:25:28 -0500

    web beta with react

---
+++ src/components/Debate.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+function Debate() {
+    return (
+      <div className="debate">
+        <div class="container">
+          <div class="row align-items-center my-5">
+            <div class="col-lg-7">
+              <img
+                class="img-fluid rounded mb-4 mb-lg-0"
+                src="http://placehold.it/900x400"
+                alt=""
+              />
+            </div>
+            <div class="col-lg-5">
+              <h1 class="font-weight-light">Db8</h1>
+              <p>
+                suck mah nuts. lets gooo
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+  
+export default Debate;
